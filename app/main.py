94a3402b5d9cbbff166e2adb4c5d06a3be5804da
Abhinav <abhinavtb@gmail.com>
2021-08-11 22:43:51 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -29,5 +29,5 @@ def predict():
         except:
             return jsonify({'error': 'error during prediction'})
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+     app.run(debug=True)
