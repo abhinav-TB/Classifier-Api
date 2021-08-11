490a7ff0367b1d11f470a021ef3c30d4592f68d6
Abhinav <abhinavtb@gmail.com>
2021-08-11 22:47:57 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,6 +2,6 @@ import requests
 
 # https://your-heroku-app-name.herokuapp.com/predict
 # http://localhost:5000/predict
-resp = requests.post("http://localhost:5000/predict", files={'file': open('ship.jpg', 'rb')})
+resp = requests.post("http://localhost:5000/predict", files={'file': open('img/ship.jpg', 'rb')})
 
 print(resp.text)
